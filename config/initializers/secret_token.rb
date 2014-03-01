3f216d3f568524ff5b1f35697963676201eb84ec
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-03-02 03:14:12 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Widgetexample::Application.config.secret_token = '34179102a9e48ed22a4bf8c39ac1d853439194b95f444356b28bd7d44f56bdff9085bb0578510df7051e071ac12c43e394fde562b7e243166697873b603a63ab'
