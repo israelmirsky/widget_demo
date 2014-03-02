f2c19e4c7f57aa44db656bfa6c9f72483fe52a7a
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-03-02 06:13:10 +0400

    statement

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,8 @@ Widgetexample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  match "statement" => 'site#statement'
+  root :to => 'site#statement'
 
   # See how all your routes lay out with "rake routes"
 
