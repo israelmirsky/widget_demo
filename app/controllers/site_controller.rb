d434b3dbf17ab136b89809807baff70d7b4878f1
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-03-18 12:45:39 +0400

    integration widget

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,3 +1,6 @@
 class SiteController < ActionController::Base
   protect_from_forgery
+
+  def invisible_widget
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Widgetexample::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   match "statement" => 'site#statement'
+  match "invisible_widget" => 'site#invisible_widget'
   root :to => 'site#statement'
 
   # See how all your routes lay out with "rake routes"
