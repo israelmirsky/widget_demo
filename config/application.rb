855297c4b8b67d3f0ee198e6da7fe10dab38025e
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-03-28 18:24:34 +0400

    precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,12 @@ module Widgetexample
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+
+    config.assets.precompile += [
+        'mobile.css'
+    ]
+
   end
 end
