257ae9661264dbcf22b6543c1cb7f71675b853d9
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-03-28 18:04:57 +0400

    fixes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,15 @@
 class SiteController < ActionController::Base
   protect_from_forgery
+  layout "site"
 
   def invisible_widget
+  end
+
+  def statement
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|Playbook|IEMobile|Nexus|Opera Mini|Mobile/i).match(request.user_agent)
+      @mobile_device = true
+    else 
+      @mobile_device = false
+    end
   end
 end
