120977956c7b6a891bf8b77ad3b3a0337fa0f0cd
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-05-29 19:40:39 +0400

    widget code refactored

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,15 +1,19 @@
 class SiteController < ActionController::Base
   protect_from_forgery
   layout "site"
+  # before_filter :mobile
 
   def invisible_widget
   end
 
-  def statement
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|Playbook|IEMobile|Nexus|Opera Mini|Mobile/i).match(request.user_agent)
-      @mobile_device = true
-    else 
-      @mobile_device = false
-    end
+  def widget
   end
+
+  # def mobile
+  #   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|Playbook|IEMobile|Nexus|Opera Mini|Mobile/i).match(request.user_agent)
+  #     @mobile_device = true
+  #   else 
+  #     @mobile_device = false
+  #   end
+  # end
 end
