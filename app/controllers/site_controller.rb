9183f40efdaa82746d93d3c058ca0d1947405f00
Vyacheslav Kozlov <v.e.kozlov@gmail.com>
2014-06-08 20:14:09 +0400

    custom design page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -9,6 +9,8 @@ class SiteController < ActionController::Base
   end
 
   def custom_design
-    @journalist = User.journalists[1]
+    @id_1 = Rails.env.production? ? User.journalists[0].uncoverage_id : "538232c25679610408210000"
+    @id_2 = Rails.env.production? ? User.journalists[1].uncoverage_id : "539308b15679618ca00a0000"
+    @id_3 = Rails.env.production? ? User.journalists[2].uncoverage_id : "5384cb2b5679612ad8260000"
   end
 end
